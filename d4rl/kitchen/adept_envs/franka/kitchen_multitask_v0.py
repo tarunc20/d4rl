fe4ca88b496a908a19bbc8975999509fc8c9c71c
Murtaza Dalal <mdalal@andrew.cmu.edu>
2020-10-26 21:43:47 -0700

    got ee control working

--- d4rl/kitchen/adept_envs/franka/kitchen_multitask_v0.py
+++ d4rl/kitchen/adept_envs/franka/kitchen_multitask_v0.py
@@ -14,13 +14,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import copy
 import os
+
+import mujoco_py
 import numpy as np
 from d4rl.kitchen.adept_envs import robot_env
 from d4rl.kitchen.adept_envs.utils.configurable import configurable
 from gym import spaces
 from dm_control.mujoco import engine
+
+from robosuite.controllers import (
+    EndEffectorImpedanceController,
+    EndEffectorInverseKinematicsController,
+)
 
 
 @configurable(pickleable=True)
@@ -42,7 +49,6 @@ class KitchenV0(robot_env.RobotEnv):
         self.obs_dict = {}
         self.robot_noise_ratio = 0.1  # 10% as per robot_config specs
         self.goal = np.zeros((30,))
-
         super().__init__(
             self.MODEl,
             robot=self.make_robot(
@@ -55,8 +61,19 @@ class KitchenV0(robot_env.RobotEnv):
                 distance=2.2, lookat=[-0.2, 0.5, 2.0], azimuth=70, elevation=-35
             ),
         )
-        self.init_qpos = self.sim.model.key_qpos[0].copy()
+        self.reset_mocap_welds(self.sim)
+        self.sim.forward()
 
+        gripper_target = np.array(
+            [-0.498, 0.005, -0.431 + 0.01]
+        ) + self.sim.data.get_site_xpos("end_effector")
+        gripper_rotation = np.array([1.0, 0.0, 1.0, 0.0])
+        self.sim.data.set_mocap_pos("mocap", gripper_target)
+        self.sim.data.set_mocap_quat("mocap", gripper_rotation)
+        for _ in range(10):
+            self.sim.step()
+
+        self.init_qpos = self.sim.model.key_qpos[0].copy()
         # For the microwave kettle slide hinge
         self.init_qpos = np.array(
             [
@@ -106,20 +123,175 @@ class KitchenV0(robot_env.RobotEnv):
         obs_lower = -obs_upper
         self.observation_space = spaces.Box(obs_lower, obs_upper)
 
+    def ctrl_set_action(self, sim, action):
+        """For torque actuators it copies the action into mujoco ctrl field.
+        For position actuators it sets the target relative to the current qpos.
+        """
+        if sim.model.nmocap > 0:
+            _, action = np.split(action, (sim.model.nmocap * 7,))
+        if sim.data.ctrl is not None:
+            for i in range(action.shape[0]):
+                if sim.model.actuator_biastype[i] == 0:
+                    sim.data.ctrl[i] = action[i]
+                else:
+                    idx = sim.model.jnt_qposadr[sim.model.actuator_trnid[i, 0]]
+                    sim.data.ctrl[i] = sim.data.qpos[idx] + action[i]
+
+    def mocap_set_action(self, sim, action):
+        """The action controls the robot using mocaps. Specifically, bodies
+        on the robot (for example the gripper wrist) is controlled with
+        mocap bodies. In this case the action is the desired difference
+        in position and orientation (quaternion), in world coordinates,
+        of the of the target body. The mocap is positioned relative to
+        the target body according to the delta, and the MuJoCo equality
+        constraint optimizer tries to center the welded body on the mocap.
+        """
+        if sim.model.nmocap > 0:
+            action, _ = np.split(action, (sim.model.nmocap * 7,))
+            action = action.reshape(sim.model.nmocap, 7)
+
+            pos_delta = action[:, :3]
+            quat_delta = action[:, 3:]
+
+            self.reset_mocap2body_xpos(sim)
+            sim.data.mocap_pos[:] = sim.data.mocap_pos + pos_delta
+            sim.data.mocap_quat[:] = sim.data.mocap_quat
+
+    def reset_mocap_welds(self, sim):
+        """Resets the mocap welds that we use for actuation."""
+        if sim.model.nmocap > 0 and sim.model.eq_data is not None:
+            for i in range(sim.model.eq_data.shape[0]):
+                if sim.model.eq_type[i] == mujoco_py.const.EQ_WELD:
+                    sim.model.eq_data[i, :] = np.array(
+                        [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+                    )
+        sim.forward()
+
+    def reset_mocap2body_xpos(self, sim):
+        """Resets the position and orientation of the mocap bodies to the same
+        values as the bodies they're welded to.
+        """
+
+        if (
+            sim.model.eq_type is None
+            or sim.model.eq_obj1id is None
+            or sim.model.eq_obj2id is None
+        ):
+            return
+        for eq_type, obj1_id, obj2_id in zip(
+            sim.model.eq_type, sim.model.eq_obj1id, sim.model.eq_obj2id
+        ):
+            if eq_type != mujoco_py.const.EQ_WELD:
+                continue
+
+            mocap_id = sim.model.body_mocapid[obj1_id]
+            if mocap_id != -1:
+                # obj1 is the mocap, obj2 is the welded body
+                body_idx = obj2_id
+            else:
+                # obj2 is the mocap, obj1 is the welded body
+                mocap_id = sim.model.body_mocapid[obj2_id]
+                body_idx = obj1_id
+
+            assert mocap_id != -1
+            sim.data.mocap_pos[mocap_id][:] = sim.data.body_xpos[body_idx]
+            sim.data.mocap_quat[mocap_id][:] = sim.data.body_xquat[body_idx]
+
+    def _set_action(self, action):
+        assert action.shape == (4,)
+        action = (
+            action.copy()
+        )  # ensure that we don't change the action outside of this scope
+        pos_ctrl, gripper_ctrl = action[:3], action[3]
+
+        pos_ctrl *= 0.05  # limit maximum change in position
+        rot_ctrl = [
+            1.0,
+            0.0,
+            1.0,
+            0.0,
+        ]  # fixed rotation of the end effector, expressed as a quaternion
+        gripper_ctrl = np.array([gripper_ctrl, gripper_ctrl])
+        assert gripper_ctrl.shape == (2,)
+        action = np.concatenate([pos_ctrl, rot_ctrl, gripper_ctrl])
+
+        # Apply action to simulation.
+        self.ctrl_set_action(self.sim, action)
+        self.mocap_set_action(self.sim, action)
+
     def _get_reward_n_score(self, obs_dict):
         raise NotImplementedError()
 
+    def grasp(self):
+        for i in range(self.skip):
+            self.sim.data.qpos[7] -= 0.0003
+            self.sim.data.qpos[8] -= 0.0003
+            self.sim.step()
+
+    def rotate_ee(self):
+        for i in range(self.skip):
+            self.sim.data.qpos[6] -= 0.01
+            self.sim.step()
+
+    def goto_pose(self, pose):
+        for i in range(self.skip):
+            self.controller.set_goal(pose)
+
     def step(self, a, b=None):
         a = np.clip(a, -1.0, 1.0)
+
+        # action = np.clip(a, self.action_space.low, self.action_space.high)
 
         if not self.initializing:
             a = self.act_mid + a * self.act_amp  # mean center and scale
         else:
             self.goal = self._get_task_goal()  # update goal if init
+            self.controller = EndEffectorImpedanceController(
+                self.sim,
+                eef_name="panda0_link7",
+                joint_indexes={
+                    "joints": list(range(9)),
+                    "qpos": list(range(9)),
+                    "qvel": list(range(9)),
+                },
+                control_ori=False,
+                policy_freq=self.skip,
+                control_delta=True,
+            )
+            # self.controller = EndEffectorInverseKinematicsController(
+            #     self.sim,
+            #     eef_name="panda0_link7",
+            #     robot_name="panda",
+            #     actuator_range=self.sim.model.actuator_ctrlrange,
+            #     joint_indexes={
+            #         "joints": list(range(9)),
+            #         "qpos": list(range(9)),
+            #         "qvel": list(range(9)),
+            #     },
+            #     ik_ori_limit=1,
+            #     ik_pos_limit=1,
+            # )
+        # a[:3] = [0, 0, 0.0]
+        # a[3:7] = [0, 0, 0, 0]
+        # self.controller.set_goal(a[:3])
+        # for i in range(self.skip):
+        #     a = self.controller.run_controller()
+        #     self.do_simulation(a, 1)
+        #
+        # for i in range(self.skip):
+        #     self.robot.step(self, self.controller, a, step_duration=1)
+        # self.sim.data.qpos[7] -= 0.001
+        # self.sim.data.qpos[6] -= 1
+        # self.sim.data.qpos[5] -= 1
+        # self.sim.step()
+        # self.grasp()
+        # print(self.get_endeff_pos())
 
-        self.robot.step(self, a, step_duration=self.skip * self.model.opt.timestep)
-
+        # print(self.get_endeff_pos())
         # observations
+        a = np.array([0, 1, 0, 0]).astype(float)
+        self._set_action(a)
+        self.sim.step()
         obs = self._get_obs()
 
         # rewards
@@ -215,11 +387,12 @@ class KitchenTaskRelaxV1(KitchenV0):
 
     def render(self, mode="human"):
         if mode == "rgb_array":
-            camera = engine.MovableCamera(self.sim, 256, 256)
-            camera.set_pose(
-                distance=2.2, lookat=[-0.2, 0.5, 2.0], azimuth=70, elevation=-35
-            )
-            img = camera.render()
+            # camera = engine.MovableCamera(self.sim, 256, 256)
+            # camera.set_pose(
+            #     distance=2.2, lookat=[-0.2, 0.5, 2.0], azimuth=70, elevation=-35
+            # )
+            # img = camera.render()
+            img = self.sim_robot.renderer.render_offscreen(1000, 1000)
             return img
         else:
             super(KitchenTaskRelaxV1, self).render()
--- d4rl/kitchen/run_kitchen.py
+++ d4rl/kitchen/run_kitchen.py
@@ -6,10 +6,17 @@ from d4rl.kitchen.kitchen_envs import KitchenMicrowaveV0
 env = KitchenMicrowaveV0(dense=True)
 env.reset()
 done = False
+ctr = 0
 while not done:
+    # if ctr % 100 == 0:
+    #     env.reset()
+    #     env.grasp()
+    # if ctr % 10 == 0:
+    #     env.rotate_ee()
+    ctr += 1
     o, r, d, i = env.step(env.action_space.sample())
     # print("microwave", i["microwave distance to goal"])
-    im = env.render(mode="human")
-    # cv2.imshow("env", im)
-    # cv2.waitKey(1)
+    im = env.render(mode="rgb_array")
+    cv2.imshow("env", im)
+    cv2.waitKey(1)
     print(r)
